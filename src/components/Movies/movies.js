1808b152498460256d329b2aa616aabb0eb8c01c
Samuel Titiloye <samuelotitiloye@gmail.com>
2019-06-22 10:57:15 -0500

    display movie posters-description-title on the dom

--- src/components/Movies/movies.js
+++ src/components/Movies/movies.js
@@ -17,9 +17,13 @@ class movies extends Component {
 
     render() {
         return (
+            ///map through my array to get movies on the dom
+            // stringify to show what is in the reduxState
             <div>
                 <>
-                {this.props.reduxState.movies.map(movie => {return <li key={movie.id}>{movie.title}</li>})};
+                {this.props.reduxState.movies.map(movie => {return <>
+                <li key={movie.id}>{movie.title}<br/>{movie.poster}<br/>{movie.description}</li>
+                <img src={movie.poster} alt="database movie posters" /> </> })};
                 </>
                 <>
                 {JSON.stringify(this.props.reduxState, null, 2)};
@@ -36,11 +40,9 @@ const mapReduxStateToProps = reduxState => ({
 
 export default connect (mapReduxStateToProps)(movies);
 
+// now that the movies show up on the dom..now what?
+//
 
-
-
-// stringify to show what is in the reduxState
-//map through my array to get movies on the dom}
 
 
 
